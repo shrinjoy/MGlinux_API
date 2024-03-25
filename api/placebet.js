fb295355343c8978b561ffa0704a75feda36b339
Noiz <shrinjoybiswas0@gmail.com>
2024-03-25 12:31:30 +0530

    Update placebet.js

--- api/placebet.js
+++ api/placebet.js
@@ -6,48 +6,55 @@
 module.exports = {
     placebet: function (db, req) {
         return new Promise((resolve, reject) => {
-            userdata = db
-                .query(
-                    `SELECT * from [playjeeto].[dbo].[CLIENTLOGIN] where CLIENTUSERNAME ='${req["username"]}' and CLIENTPASSWORD='${req["password"]}'`
-                )
-                .then((data) => {
-                    //check if user has enough balance;
-                    if (data.recordset[0].CLIENTBALANCE > req["totalbet"]) {
-                        //deduct balance from user wallet
-                        db.query(
-                            `UPDATE [playjeeto].[dbo].[CLIENTLOGIN]  set CLIENTBALANCE  = CLIENTBALANCE -${req["totalbet"]}`
-                        )
-                            .then((data) => {
-                                
-                                 db.query("SELECT FORMAT(GETDATE(), 'yyyyMMddHHmmssfff') as barcode")
+
+            db.query(`SELECT DATEDIFF(SECOND,  GETDATE(),CONVERT(DATETIME, NEXTDRAW, 109)) AS timer FROM dbo.TARMINALTIMEZONE;`).then((data) => {
+                if(data.recordset[0].timer >10)
+                userdata = db
+                    .query(
+                        `SELECT * from [playjeeto].[dbo].[CLIENTLOGIN] where CLIENTUSERNAME ='${req["username"]}' and CLIENTPASSWORD='${req["password"]}'`
+                    )
+                    .then((data) => {
+                        //check if user has enough balance;
+                        if (data.recordset[0].CLIENTBALANCE > req["totalbet"]) {
+                            //deduct balance from user wallet
+                            db.query(
+                                `UPDATE [playjeeto].[dbo].[CLIENTLOGIN]  set CLIENTBALANCE  = CLIENTBALANCE -${req["totalbet"]}`
+                            )
                                 .then((data) => {
-                                    var barcodedata =  "B"+data.recordset[0]["barcode"];
-                                
-                                    var querystring = `INSERT INTO [playjeeto].[dbo].[TICKET99] (TICKETNUMBER,TICKETDETAILS,TICKETRS,TICKETTOTALRS,GAMEDATE,GAMETIME,TARMINALID,GAMEID) 
-                                    VALUES ('${barcodedata}','${req["tickets"]}',1,${req["totalbet"]},FORMAT(GETDATE(), 'yyyy-MM-dd'),FORMAT(GETDATE(), 'yyyy-MM-dd HH:mm:ss'),'${req["username"]}','${req["gameid"]}');`;
-                                    db.query(querystring)
+
+                                    db.query("SELECT FORMAT(GETDATE(), 'yyyyMMddHHmmssfff') as barcode")
                                         .then((data) => {
-                                            resolve({ "barcode": barcodedata,"message":"placed bet "});
+                                            var barcodedata = "B" + data.recordset[0]["barcode"];
+
+                                            var querystring = `INSERT INTO [playjeeto].[dbo].[TICKET99] (TICKETNUMBER,TICKETDETAILS,TICKETRS,TICKETTOTALRS,GAMEDATE,GAMETIME,TARMINALID,GAMEID) 
+                                    VALUES ('${barcodedata}','${req["tickets"]}',1,${req["totalbet"]},FORMAT(GETDATE(), 'yyyy-MM-dd'),FORMAT(GETDATE(), 'yyyy-MM-dd HH:mm:ss'),'${req["username"]}','${req["gameid"]}');`;
+                                            db.query(querystring)
+                                                .then((data) => {
+                                                    resolve({ "barcode": barcodedata, "message": "placed bet " });
+                                                })
+                                                .catch((err) => {
+                                                    reject({ error: `${err}` });
+                                                });
+
                                         })
                                         .catch((err) => {
                                             reject({ error: `${err}` });
-                                        });
-
+                                        })
                                 })
                                 .catch((err) => {
-                                reject({ error: `${err}` });
-                                })
-                            })
-                            .catch((err) => {
-                                reject({ message: "failed to place bet because- " + err });
-                            });
-                    } else {
-                        reject({ message: "failed to place bet because of low balance" });
-                    }
-                })
-                .catch((err) => {
-                    reject({ message: "failed to place bets because " + err });
-                });
-        });
+                                    reject({ message: "failed to place bet because- " + err });
+                                });
+                        } else {
+                            reject({ message: "failed to place bet because of low balance" });
+                        }
+                    })
+                    .catch((err) => {
+                        reject({ message: "failed to place bets because " + err });
+                    });
+            });
+        }
+        )
     },
 };
+//SELECT DATEDIFF(SECOND,  GETDATE(),CONVERT(DATETIME, NEXTDRAW, 109)) AS timer FROM dbo.TARMINALTIMEZONE;
+//sql.query(`SELECT GAMEID,TARMINALDATE AS NEXTGAMEDATE,TARMINALTIME AS NEXTGAMETIME,NEXTDRAW,DATEDIFF(SECOND,  GETDATE(),CONVERT(DATETIME, NEXTDRAW, 109)) AS timer FROM dbo.TARMINALTIMEZONE;`)
