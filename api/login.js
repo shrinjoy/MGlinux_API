267d2c84b18e65b38755f94486e90abb29337fa8
Noiz <shrinjoybiswas0@gmail.com>
2024-04-28 13:26:35 +0530

    Revert "Update login.js"
    This reverts commit bcc6f90286b654dff6f5acae6c218b95e8695150.

--- api/login.js
+++ api/login.js
@@ -2,27 +2,16 @@ module.exports = {
     canlogin:function (db,request)
     {
         return new Promise((resolve ,reject)=>{
-
-
-            
-           db.query(`SELECT * from [playjeeto].[dbo].[CLIENTLOGIN] where CLIENTID ='${request["userid"].toString().trim()}'`).then((data)=>{
+           db.query(`SELECT * from [playjeeto].[dbo].[CLIENTLOGIN] where CLIENTID ='${request["userid"]}' and CLIENTUSERNAME ='${request["username"]}' and CLIENTPASSWORD='${request["password"]}'`).then((data)=>{
                if(data.recordset.length<1)
                {
                    reject({"message":"no user found with the provided creds"});
                }
                else
                {
-                console.log(data.recordset[0]["CLIENTUSERNAME"].toString().trim()===request["username"].toString().trim());
-                console.log(data.recordset[0]["CLIENTPASSWORD"]);
-                
-                if(data.recordset[0]["CLIENTUSERNAME"].toString().trim()===request["username"].toString().trim() && data.recordset[0]["CLIENTPASSWORD"].toString().trim()===request["password"].toString().trim()){
                  
                         resolve({"message": "good to log in "})
-                }
-                else
-                {
-                    reject({"message":"wrong username or password"})
-                }
+                    
                     
                }
 
@@ -34,7 +23,7 @@ module.exports = {
     canlogin_macid:function (db,request)
     {
         return new Promise((resolve ,reject)=>{
-           db.query(`SELECT * from [playjeeto].[dbo].[CLIENTLOGIN] where CLIENTID ='${request["userid"].toString().trim()}' and CLIENTUSERNAME ='${request["username"].toString().trim()}' and CLIENTPASSWORD='${request["password"].toString().trim()}'`).then((data)=>{
+           db.query(`SELECT * from [playjeeto].[dbo].[CLIENTLOGIN] where CLIENTID ='${request["userid"]}' and CLIENTUSERNAME ='${request["username"]}' and CLIENTPASSWORD='${request["password"]}'`).then((data)=>{
                
             
             
