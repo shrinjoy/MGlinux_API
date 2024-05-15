35529a7fa1317d1ea52ed23d6b9d86ab6b4bb370
webviosystem10 <riddhee25@gmail.com>
2024-05-15 17:25:55 +0530

    .

--- api/mgdelux_linux_template/src/Pages/Home.js
+++ api/mgdelux_linux_template/src/Pages/Home.js
@@ -30,28 +30,30 @@ function Home() {
                         Exit
                     </button>
                 </div>
-                {isConnected ? <div className="status">CONNECTED</div> : ""}
-                {networkError ? <div className="status text-danger">NETWORK ERROR</div> : ""}
+                <div className="unlockForm">
+                    {isConnected ? <div className="status">CONNECTED</div> : ""}
+                    {networkError ? <div className="status text-danger">NETWORK ERROR</div> : ""}</div>
+                <div className="buttonGroup row">
+                    <div style={{ width: '20%' }}>
+                        {isConnected ? <button className="homeBtn" onClick={() => { setIsConnected(false) }}>Dis-Connect</button> : <button className="homeBtn connect" onClick={() => checkInternetConnection()}>
+                            Connect
+                        </button>}
+                    </div>
+                    <div style={{ width: '20%' }}>
+                        {isConnected ? <button className="homeBtn login" onClick={() => navigate('/login')}>Login</button> : ""}
+                    </div>
+                    <div style={{ width: '20%' }}>
+                        <button className="homeBtn" onClick={() => { window.electronAPI.systemSettings(); window.open('http://localhost:631/admin', '_blank') }}>System</button>
+                    </div>
+                    <div style={{ width: '20%' }}>
+                        <button className="homeBtn" onClick={() => systemServGet("restart")}>Restart</button>
+                    </div>
+                    <div style={{ width: '20%' }}>
+                        <button className="homeBtn" onClick={() => systemServGet("shutdown")}>PowerOff</button>
+                    </div>
+                </div>
             </div>
-            <div className="buttonGroup row">
-                <div style={{ width: '20%' }}>
-                    {isConnected ? <button className="homeBtn" onClick={() => { setIsConnected(false) }}>Dis-Connect</button> : <button className="homeBtn connect" onClick={() => checkInternetConnection()}>
-                        Connect
-                    </button>}
-                </div>
-                <div style={{ width: '20%' }}>
-                    {isConnected ? <button className="homeBtn login" onClick={() => navigate('/login')}>Login</button> : ""}
-                </div>
-                <div style={{ width: '20%' }}>
-                    <button className="homeBtn" onClick={() => { window.electronAPI.systemSettings(); window.open('http://localhost:631/admin', '_blank') }}>System</button>
-                </div>
-                <div style={{ width: '20%' }}>
-                    <button className="homeBtn" onClick={() => systemServGet("restart")}>Restart</button>
-                </div>
-                <div style={{ width: '20%' }}>
-                    <button className="homeBtn" onClick={() => systemServGet("shutdown")}>PowerOff</button>
-                </div>
-            </div>
+
         </main >
 
     )
