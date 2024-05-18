b0aad4bb67e127757343c6443b238e3b7057ff38
webviosystem10 <riddhee25@gmail.com>
2024-05-18 11:20:28 +0530

    .

---
+++ api/playjeetowebsite/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='*' element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
