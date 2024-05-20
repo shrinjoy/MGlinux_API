32bd2986f1c540dd7a043a68fdf1471abf3eaf7c
webviosystem10 <riddhee25@gmail.com>
2024-05-20 14:02:34 +0530

    .

--- api/playjeetowebsite/src/Pages/Home.js
+++ api/playjeetowebsite/src/Pages/Home.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom'
 function Home() {
     return (
         <main>
-            <Navbar />
+            {/* <Navbar /> */}
             <section className="HomeBanner">
                 <div className='container'>
                     <div className='row g-3'>
