723c0154bb16f070f4589a889353b6e71b9eb664
webviosystem10 <webviosystem10@gmail.com>
2024-05-21 15:58:16 +0530

    .

--- api/playjeetowebsite/reverseProxy.js
+++ api/playjeetowebsite/reverseProxy.js
@@ -1,15 +1,11 @@
 const http = require('http');
 const httpProxy = require('http-proxy');
 
-// Target URL where the requests will be forwarded
 const target = 'http://playjeeto.in:8083';
 
-// Create a proxy server instance
 const proxy = httpProxy.createProxyServer({});
 
-// Create a basic HTTP server
 const server = http.createServer((req, res) => {
-    // Redirect requests to the target URL
     proxy.web(req, res, { target }, (err) => {
         console.error('Proxy error:', err);
         res.writeHead(500, {
@@ -19,7 +15,6 @@ const server = http.createServer((req, res) => {
     });
 });
 
-// Handle proxy errors
 proxy.on('error', (err, req, res) => {
     console.error('Proxy server error:', err);
     res.writeHead(500, {
@@ -28,7 +23,6 @@ proxy.on('error', (err, req, res) => {
     res.end('Proxy server error');
 });
 
-// Start the server on port 80
 const port = 80;
 server.listen(port, () => {
     console.log(`Reverse proxy server listening on port ${port}`);
