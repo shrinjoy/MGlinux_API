5c267ff2b41efb5acf9c0f56885e2ce01d6fd9fd
webviosystem10 <webviosystem10@gmail.com>
2024-05-21 12:19:36 +0530

    .

--- api/mgdelux_linux_template/src/Pages/Components/TicketToPrint.js
+++ api/mgdelux_linux_template/src/Pages/Components/TicketToPrint.js
@@ -2,6 +2,18 @@ import React, { forwardRef, useContext, useEffect, useState } from 'react';
 import Barcode from 'react-barcode'
 import { DataContext } from '../../Context/DataContext';
 
+const dateTimeTicketFormatter = (dateTime) => {
+    const ogdate = dateTime.split('T')[0];
+    const ogtime = dateTime.split('T')[1].split('.')[0];
+
+    const formattedDate = ogdate.replace(/-/g, '');
+    const formattedTime = ogtime.replace(/:/g, '');
+
+    const combinedDateTime = formattedDate + formattedTime;
+
+    return { combinedDateTime, ogtime }
+}
+
 const TicketToPrint = forwardRef((props, ref) => {
     const { isClaim } = props;
     const { ticketData } = useContext(DataContext)
@@ -12,12 +24,6 @@ const TicketToPrint = forwardRef((props, ref) => {
         setTicketArr(tempTicketArr);
     }, [])
 
-    const dateTimeTicketFormatter = (date, time) => {
-        const ogdate = date;
-        const ogtime = time;
-
-
-    }
     return (
         <div ref={ref} className='ticket'>
             <div className='container'>
@@ -26,25 +32,25 @@ const TicketToPrint = forwardRef((props, ref) => {
                         <h3>Metro Deluxe</h3>
                     </div>
                     <div className='col-6 text-start'>
-                        <div><label>Ack: {ticketData.barcode.slice(0, 6)}</label></div>
-                        <div><label>{ticketData.gameID}: {new Date(ticketData.gameDate).toLocaleDateString()}-{new Date(ticketData.gameTime).toLocaleTimeString('en-US', { timeZone: 'UTC' })}</label></div>
+                        <div><label>Ack: {ticketData?.barcode.slice(0, 6)}</label></div>
+                        <div><label>{ticketData?.gameID}: {dateTimeTicketFormatter(ticketData?.gameTime).combinedDateTime}</label></div>
                     </div>
                     <div className='col-6 text-end'>
-                        <div><label>PosID: {ticketData.posID}</label></div>
+                        <div><label>PosID: {ticketData?.posID}</label></div>
                         <div><label>STONE GROUP: MG</label></div>
                     </div>
                     <div className='listType'>
                         <ul>
-                            {ticketArr ? ticketArr.map((item, index) => (
+                            {ticketArr?.map((item, index) => (
                                 <li key={index}>{item.slice(0, 4)} * {item.slice(5)}</li>
-                            )) : ""}
+                            ))}
                         </ul>
                     </div>
                     <div className='text-center'>
-                        <label>Qty: {ticketData.totalQty} Total Pts: {ticketData.totalQty} {new Date(ticketData.drawTime).toLocaleTimeString('en-US', { timeZone: 'UTC' })}</label>
+                        <label>Qty: {ticketData?.totalQty} Total Pts: {ticketData?.totalQty} {dateTimeTicketFormatter(ticketData?.gameTime).ogtime}</label>
                     </div>
                     <div className='text-center'>
-                        <Barcode value={ticketData.barcode} height={60} width={1} />
+                        <Barcode value={ticketData?.barcode} height={60} width={1} />
                     </div>
                 </div> :
                     <div className='row'>
@@ -52,14 +58,14 @@ const TicketToPrint = forwardRef((props, ref) => {
                             <h3>Claimed</h3>
                         </div>
                         <div className='col-12 text-start'>
-                            <div><label>Metro Deluxe {new Date(ticketData.gameDate).toLocaleDateString()}-{new Date(ticketData.gameTime).toLocaleTimeString('en-US', { timeZone: 'UTC' })}</label></div>
-                            <div><label>{ticketData.posID} Bar: {ticketData.barcode}</label></div>
+                            <div><label>Metro Deluxe {dateTimeTicketFormatter(ticketData?.gameTime).combinedDateTime}</label></div>
+                            <div><label>{ticketData?.posID} Bar: {ticketData?.barcode}</label></div>
                         </div>
                         <div className='col-6'>
                             <div><label>Prize Claim Points</label></div>
                         </div>
                         <div className='col-6'>
-                            <div><label>{ticketData.winPoints} Pts.</label></div>
+                            <div><label>{ticketData?.winPoints} Pts.</label></div>
                         </div>
                     </div>}
             </div>
