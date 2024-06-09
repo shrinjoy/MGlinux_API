724d6439ab02924471989901a6f8c01dc390d8f3
GTPang <riddhee25@gmail.com>
2024-06-09 19:34:43 +0530

    .

--- api/mgdelux_linux_template/src/Pages/Home.js
+++ api/mgdelux_linux_template/src/Pages/Home.js
@@ -1,44 +1,44 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import { systemServGet } from '../Globals/GlobalFunctions';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { systemServGet } from "../Globals/GlobalFunctions";
 
 function Home() {
-    const [clickConnect, setClickConnect] = useState(false);
-    const [isConnected, setIsConnected] = useState(false);
-    const [networkError, setNetworkError] = useState(false);
-    const [error, setError] = useState(false);
+  const [clickConnect, setClickConnect] = useState(false);
+  const [isConnected, setIsConnected] = useState(false);
+  const [networkError, setNetworkError] = useState(false);
+  const [error, setError] = useState(false);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    async function checkInternetConnection() {
-        const res = await window.electronAPI.checkInternet();
-        if (res) {
-            setIsConnected(true);
-            setNetworkError(false);
-        } else {
-            setIsConnected(false);
-            setNetworkError(true);
-        }
+  async function checkInternetConnection() {
+    const res = await window.electronAPI.checkInternet();
+    if (res) {
+      setIsConnected(true);
+      setNetworkError(false);
+    } else {
+      setIsConnected(false);
+      setNetworkError(true);
     }
+  }
 
-    const triggerDriverChecker = () => {
-        window.electronAPI.printDriver();
-        console.log('test');
+  const triggerDriverChecker = () => {
+    window.electronAPI.printDriver();
+    console.log("test");
+  };
+
+  const handleLoginNavigation = () => {
+    if (isConnected && !networkError) {
+      navigate("/login");
+    } else {
+      setError(true);
+      setTimeout(() => setError(false), 2500);
     }
+  };
 
-    const handleLoginNavigation = () => {
-        if (isConnected && !networkError) {
-            navigate('/login')
-        } else {
-            setError(true);
-            setTimeout(() => setError(false), 2500)
-        }
-    }
-
-    return (
-        <main className="homeWrapper">
-            <div className="wrapper p-5">
-                {/* <div className='position-absolute' style={{ top: 10, right: 50 }}>
+  return (
+    <main className="homeWrapper">
+      <div className="wrapper p-5">
+        {/* <div className='position-absolute' style={{ top: 10, right: 50 }}>
                     <button
                         onClick={() => window.electronAPI.quitApp()}
                         className="homeBtn"
@@ -46,36 +46,79 @@ function Home() {
                         Exit
                     </button>
                 </div> */}
-                <div className="unlockForm">
-                    {error ? <div className="status">PLEASE CONNECT FIRST</div> : ""}
-                    {isConnected ? <div className="status">CONNECTED</div> : ""}
-                    {networkError ? <div className="status text-danger">NETWORK ERROR</div> : ""}</div>
-                <div className="buttonGroup row">
-                    <div style={{ width: '16%' }}>
-                        {isConnected ? <button className="homeBtn" onClick={() => { setIsConnected(false) }}>Dis-Connect</button> : <button className="homeBtn connect" onClick={() => checkInternetConnection()}>
-                            Connect
-                        </button>}
-                    </div>
-                    <div style={{ width: '16%' }}>
-                        <button className="homeBtn login transparent" onClick={handleLoginNavigation}>Login</button>
-                    </div>
-                    <div style={{ width: '16%' }}>
-                        <button className="homeBtn transparent" onClick={() => window.electronAPI.systemSettings()}>System</button>
-                    </div>
-                    <div style={{ width: '16%' }}>
-                        <button className="homeBtn transparent" onClick={() => window.electronAPI.printDriver()}>Init</button>
-                    </div>
-                    <div style={{ width: '16%' }}>
-                        <button className="homeBtn transparent" onClick={() => window.electronAPI.systemRestart()}>Restart</button>
-                    </div>
-                    <div style={{ width: '16%' }}>
-                        <button className="homeBtn transparent" onClick={() => window.electronAPI.systemShutdown()}>PowerOff</button>
-                    </div>
-                </div>
-            </div>
-        </main >
-
-    )
+        <div className="unlockForm">
+          {error ? <div className="status">PLEASE CONNECT FIRST</div> : ""}
+          {isConnected ? <div className="status">CONNECTED</div> : ""}
+          {networkError ? (
+            <div className="status text-danger">ERROR 1001</div>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className="buttonGroup row">
+          <div style={{ width: "16%" }}>
+            {isConnected ? (
+              <button
+                className="homeBtn"
+                onClick={() => {
+                  setIsConnected(false);
+                }}
+              >
+                Dis-Connect
+              </button>
+            ) : (
+              <button
+                className="homeBtn connect"
+                onClick={() => checkInternetConnection()}
+              >
+                Connect
+              </button>
+            )}
+          </div>
+          <div style={{ width: "16%" }}>
+            <button
+              className="homeBtn login transparent"
+              onClick={handleLoginNavigation}
+            >
+              Login
+            </button>
+          </div>
+          <div style={{ width: "16%" }}>
+            <button
+              className="homeBtn transparent"
+              onClick={() => window.electronAPI.systemSettings()}
+            >
+              System
+            </button>
+          </div>
+          <div style={{ width: "16%" }}>
+            <button
+              className="homeBtn transparent"
+              onClick={() => window.electronAPI.printDriver()}
+            >
+              Init
+            </button>
+          </div>
+          <div style={{ width: "16%" }}>
+            <button
+              className="homeBtn transparent"
+              onClick={() => window.electronAPI.systemRestart()}
+            >
+              Restart
+            </button>
+          </div>
+          <div style={{ width: "16%" }}>
+            <button
+              className="homeBtn transparent"
+              onClick={() => window.electronAPI.systemShutdown()}
+            >
+              PowerOff
+            </button>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
-export default Home
+export default Home;
