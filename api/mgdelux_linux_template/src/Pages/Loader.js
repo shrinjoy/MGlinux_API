cd81f7f0df558c70548ad55d27668e05771f3f2d
GTPang <riddhee25@gmail.com>
2024-06-24 21:56:45 +0530

    .

--- api/mgdelux_linux_template/src/Pages/Loader.js
+++ api/mgdelux_linux_template/src/Pages/Loader.js
@@ -1,34 +1,35 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Loader() {
-    const [value, setValue] = useState(0)
-    const navigate = useNavigate();
+  const [value, setValue] = useState(0);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setValue(prevValue => {
-                const newValue = prevValue + 10;
-                if (newValue > 85) {
-                    clearInterval(intervalId);
-                    navigate('/home')
-                }
-                return newValue;
-            });
-        }, 1000);
-        return () => clearInterval(intervalId);
-    }, []);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setValue((prevValue) => {
+        const newValue = prevValue + 10;
+        if (newValue > 100) {
+          clearInterval(intervalId);
+          navigate("/home");
+        }
+        return newValue;
+      });
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
 
-    const divs = Array.from({ length: value / 10 }, (_, index) => <div key={index}></div>)
-    return (
-        <div style={{ width: '100vw', height: '100vh', backgroundColor: '#000' }}>
-            <div className='spinner'>
-                {divs}
-                <label>{value}%</label>
-            </div>
-        </div>
-    )
+  const divs = Array.from({ length: value / 10 }, (_, index) => (
+    <div key={index}></div>
+  ));
+  return (
+    <div style={{ width: "100vw", height: "100vh", backgroundColor: "#000" }}>
+      <div className="spinner">
+        {divs}
+        <label>{value}%</label>
+      </div>
+    </div>
+  );
 }
 
-export default Loader
+export default Loader;
