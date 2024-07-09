ad5b035c7648957b789b1118b47ae30aca966cf2
Noiz <shrinjoybiswas0@gmail.com>
2024-07-09 01:43:05 -0700

    Update cancelbybarcode.js

--- mglinuxAPI/cancelbybarcode.js
+++ mglinuxAPI/cancelbybarcode.js
@@ -3,12 +3,12 @@ module.exports = {
 
         return new Promise((resolve, reject) => {
 
-            sql.query(`UPDATE [TICKET99] set TARMINALCLS = 'CANCEL' where TICKETNUMBER= '${req["barcode"]}' and TARMINALCLS !='NOTWIN' and TARMINALCLS !='WIN' and TARMINALID='${username}'`)
+            sql.query(`UPDATE [TICKET99] set TARMINALCLS = 'CANCEL' where TICKETNUMBER= '${req["barcode"]}' and TARMINALCLS !='NOTWIN' and TARMINALCLS !='WIN' `)
                 .then((data) => {
 
 
                     console.log(`SELECT * FROM [TICKET99] where TICKETNUMBER ='${req["barcode"]}'`);
-                    sql.query(`SELECT * FROM [TICKET99] where TICKETNUMBER ='${req["barcode"]}' and TARMINALID='${username}`).then((balancedata) => {
+                    sql.query(`SELECT * FROM [TICKET99] where TICKETNUMBER ='${req["barcode"]}'`).then((balancedata) => {
                         if (balancedata.recordset.length > 0) {
                             console.log(data);
                             var balancetoadd = balancedata.recordset[0]["TICKETTOTALRS"];
@@ -16,7 +16,7 @@ module.exports = {
                             var username = balancedata.recordset[0]["TARMINALID"];
                             sql.query(`UPDATE [CLIENTLOGIN] set CLIENTBALANCE  = CLIENTBALANCE+${balancetoadd} where CLIENTUSERNAME='${username}'`).then((data) => {
                                 console.log("added this balance:" + balancedata);
-                                sql.query(`UPDATE [TICKET99] set TARMINALCLS = 'CANCEL' where TICKETNUMBER= '${req["barcode"]}' and TARMINALID='${username}`).then((d2)=>{
+                                sql.query(`UPDATE [TICKET99] set TARMINALCLS = 'CANCEL' where TICKETNUMBER= '${req["barcode"]}'`).then((d2)=>{
                                 resolve({ "message": "canceled bet" });
 
                                 })
