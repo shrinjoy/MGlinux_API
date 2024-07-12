f5850104c0195c1e98f067523f6c88552b550ffe
Noiz <shrinjoybiswas0@gmail.com>
2024-07-12 09:23:25 +0530

    Update getsalesreport.js

--- mglinuxAPI/getsalesreport.js
+++ mglinuxAPI/getsalesreport.js
@@ -10,13 +10,41 @@ module.exports = {
         ISNULL(SUM(CASE WHEN [TARMINALCLS] = 'CLAIMED' THEN [WINRS] ELSE 0 END), 0) AS claimpoints,
         ISNULL(SUM([TICKETTOTALRS]), 0)
         - ISNULL(SUM(CASE WHEN [TARMINALCLS] = 'CANCEL' THEN [TICKETTOTALRS] ELSE 0 END), 0)
-        - ISNULL(SUM(CASE WHEN [TARMINALCLS] = 'CLAIMED' THEN [WINRS] ELSE 0 END), 0) AS netplaypoints
+        - ISNULL(SUM(CASE WHEN [TARMINALCLS] = 'CLAIMED' THEN [WINRS] ELSE 0 END), 0) AS optpoints
         FROM TICKET99
         WHERE 
         TARMINALID = '${req['userid']}' 
         AND GAMEDATE BETWEEN '${req['startdate']}'  AND '${req['enddate']}' ;`)
                 .then((data) => {
-                    resolve(data.recordsets[0]);
+
+
+                    db.query(`SELECT * FROM [NRDELUXE].[dbo].[CLIENTLOGIN] where CLIENTUSERNAME = '${req['userid']}' `).then((data2)=>{
+
+
+                        var discountpoints= data.recordset[0]["optpoints"] * (data2.recordset[0]["CLIENTPARSENT"]/100);
+                   
+                        var netplay = data.recordset[0]["optpoints"]-discountpoints;
+                        var finaldata = {
+                            "playpoint": data.recordset[0]["playpoint"],
+                            "cancelpoint":data.recordset[0]["cancelpoint"],
+                            "netpoint": data.recordset[0]["netpoint"],
+                            "claimpoints": data.recordset[0]["claimpoints"],
+                            "optpoints": data.recordset[0]["optpoints"],
+                            "discountpoints":Math.floor(discountpoints),
+                            "netplaypoints":Math.floor(netplay)
+                          }
+    
+                        resolve(finaldata);
+
+
+                    })
+                    .catch((err) => {
+
+                        reject(err);
+    
+                    })
+                    
+
                 })
                 .catch((err) => {
 
