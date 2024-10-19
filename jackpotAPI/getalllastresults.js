3e948f9063b1017c9ceeaf72644ed20e8e6c934f
Noiz <shrinjoybiswas0@gmail.com>
2024-10-20 02:20:29 +0530

    Update getalllastresults.js

--- jackpotAPI/getalllastresults.js
+++ jackpotAPI/getalllastresults.js
@@ -2,7 +2,7 @@ module.exports =
 {
     getlastresults_all_sofar: function (db) {
         return new Promise((resolve, reject) => {
-           
+
             db.query(`SELECT * FROM [RESULT99] WHERE CONVERT(DATE, GAMEDATE) = CONVERT(DATE,GETDATE())  order by INTNUMBER desc`)
                 .then((data) => {
                     var arraydata = {};
@@ -15,13 +15,13 @@ module.exports =
                 })
                 .catch((err) => {
                     console.log(err);
-                    reject({"message": err});
+                    reject({ "message": err });
                 });
         });
     },
     getlastresultsinfo: function (db) {
         return new Promise((resolve, reject) => {
-           
+
             db.query(`SELECT * FROM [RESULT99] WHERE CONVERT(DATE, GAMEDATE) = CONVERT(DATE,GETDATE())  order by INTNUMBER desc`)
                 .then((data) => {
                     var arraydata = [];
@@ -34,7 +34,7 @@ module.exports =
                 })
                 .catch((err) => {
                     console.log(err);
-                    reject({"message": err});
+                    reject({ "message": err });
                 });
         });
     },
@@ -46,54 +46,49 @@ module.exports =
                 }
                 else if (data.recordset.length > 0) {
                     var alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']
-                    try{
+                    try {
                         var filteredresultdata = "{"
-                    for (x = 0; x < alphabets.length; x++) {
-                        for (y = 0; y < 12; y++) {
-                            if (y < 10) {
+                        for (x = 0; x < alphabets.length; x++) {
+                            for (y = 0; y < 12; y++) {
+                                if (y < 10) {
 
-                                var datax = data.recordset[0][`${alphabets[x]}0${y}`]
+                                    var datax = data.recordset[0][`${alphabets[x]}0${y}`]
 
-                                if(datax === null)
-                                {
-                                    datax= "    "
+                                    if (datax === null) {
+                                        datax = "    "
+
+                                    }
+                                    filteredresultdata += (`"${alphabets[x]}0${y}"` + ":" + `"${datax}"`)
+                                    filteredresultdata += ","
+                                }
+                                else {
+                                    var datax = data.recordset[0][`${alphabets[x]}${y}`]
+
+                                    if (datax === null) {
+                                        datax = "    "
+
+
+
+                                    }
+                                    filteredresultdata += (`"${alphabets[x]}${y}"` + ":" + `"${datax}"`)
+
+                                    if (x == alphabets.length - 1 && y == 11) {
+                                        filteredresultdata += "}"
+
+                                    }
+                                    else {
+                                        filteredresultdata += ","
+                                    }
 
                                 }
-                                filteredresultdata+=(`"${alphabets[x]}0${y}"`+":"+`"${datax}"`)
-                                filteredresultdata+=","
-                            }
-                            else {
-                                var datax = data.recordset[0][`${alphabets[x]}${y}`]
-
-                                if(datax=== null)
-                                {
-                                    datax= "    "
-
-
-
-                                }
-                                filteredresultdata+=(`"${alphabets[x]}${y}"`+":"+`"${datax}"`)
-
-                                if(x==alphabets.length-1 && y == 11)
-                                {
-                                filteredresultdata+="}"
-
-                                }
-                                else
-                                {
-                                filteredresultdata+=","
-                                }
-
                             }
                         }
-                    }
-                    resolve(filteredresultdata);
+                        resolve(filteredresultdata);
 
-                }
-                catch(err)
-                {
-                    reject({"message":"failed to filter"+err})
-                }
+                    }
+                    catch (err) {
+                        reject({ "message": "failed to filter" + err })
+                    }
                 }
             })
                 .catch((error) => {
@@ -105,47 +100,49 @@ module.exports =
 
 
     //comibes the reuslt and muliplier and returns it only gives the result with current gamedate 
-    getonlylastresultwithmultiplier:function(db,jsondata)
-    {
+    getonlylastresultwithmultiplier: function (db, jsondata) {
         return new Promise(async (resolve, reject) => {
-           var multiplier=0;
-           var result=0;
-           await db.query(`SELECT * FROM [XRESULT] WHERE CONVERT(DATE, GDATE) = CONVERT(DATE,GETDATE())  and GDETAILS='${jsondata["gameid"]}'`)
+            var multiplier = 0;
+            var result = 0;
+            await db.query(`SELECT * FROM [XRESULT] WHERE CONVERT(DATE, GDATE) = CONVERT(DATE,GETDATE())  and GDETAILS='${jsondata["gameid"]}'`)
                 .then((data) => {
-                   if(data.recordset.length>0)
-                   {
-                    console.log(data.recordset[0]);
+                    if (data.recordset.length > 0) {
+                        console.log(data.recordset[0]);
                         multiplier = data.recordset[0].XDETAILS;
-                        console.log("multiplier value:"+multiplier)
-                   }
+                        console.log("multiplier value:" + multiplier)
+                    }
+                    else {
+                        reject({ "reject": "not available" });
+                    }
                 })
                 .catch((err) => {
                     console.log(err);
-                    reject({"message": err});
+                    reject({ "message": err });
                 });
-                await   db.query(`SELECT * FROM [RESULT99] WHERE CONVERT(DATE, GAMEDATE) = CONVERT(DATE,GETDATE()) and GAMENUM='${jsondata["gameid"]}' order by INTNUMBER desc`)
+            await db.query(`SELECT * FROM [RESULT99] WHERE CONVERT(DATE, GAMEDATE) = CONVERT(DATE,GETDATE()) and GAMENUM='${jsondata["gameid"]}' order by INTNUMBER desc`)
                 .then((data) => {
-                    if(data.recordset.length>0)
-                        {
-                           
-                            result = data.recordset[0].GAMERESULT;
-                            console.log("result value:"+result)
-     
-                        }
+                    if (data.recordset.length > 0) {
+
+                        result = data.recordset[0].GAMERESULT;
+                        console.log("result value:" + result)
+
+                    }
+                    else {
+                        reject({ "reject": "not available" });
+                    }
                 })
                 .catch((err) => {
                     console.log(err);
-                    reject({"message": err});
-                });    
-                resolve({"muliplier":`${multiplier}`,"result":`${result}`});
-            
+                    reject({ "message": err });
+                });
+            resolve({ "muliplier": `${multiplier}`, "result": `${result}` });
+
         });
     },
 
-    getlastresultwithmultiplier:function(db,jsondata)
-    {
+    getlastresultwithmultiplier: function (db, jsondata) {
         return new Promise((resolve, reject) => {
-           
+
             db.query(`SELECT * FROM [XRESULT] WHERE CONVERT(DATE, GDATE) = CONVERT(DATE,GETDATE())  and GDETAILS='${jsondata["gameid"]}'`)
                 .then((data) => {
                     var arraydata = [];
@@ -158,14 +155,13 @@ module.exports =
                 })
                 .catch((err) => {
                     console.log(err);
-                    reject({"message": err});
+                    reject({ "message": err });
                 });
         });
     },
-    getallresultwithmultiplier:function(db)
-    {
+    getallresultwithmultiplier: function (db) {
         return new Promise((resolve, reject) => {
-           //GDATE
+            //GDATE
             db.query(`SELECT * FROM [XRESULT] WHERE CONVERT(DATE, GDATE) = CONVERT(DATE,GETDATE())`)
                 .then((data) => {
                     var arraydata = [];
@@ -178,7 +174,7 @@ module.exports =
                 })
                 .catch((err) => {
                     console.log(err);
-                    reject({"message": err});
+                    reject({ "message": err });
                 });
         });
     }
