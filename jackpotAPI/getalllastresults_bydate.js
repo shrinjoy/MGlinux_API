8d4e7f38e7eb6b9a7dc691f2393ec2ce28e80eb4
Noiz <shrinjoybiswas0@gmail.com>
2024-10-30 23:22:57 -0700

    update

--- jackpotAPI/getalllastresults_bydate.js
+++ jackpotAPI/getalllastresults_bydate.js
@@ -1,21 +1,29 @@
 module.exports = {
     getlastresults_all_bydate: function (db, json) {
         return new Promise((resolve, reject) => {
-           
-            db.query(`SELECT * FROM [RESULT99] WHERE CONVERT(DATE, GAMEDATE) = CONVERT(DATE, '${json["date"]}')   order by INTNUMBER;`)
-            .then((data) => {
-                var arraydata = {};
-                console.log(data.recordset.length);
-                data.recordset.forEach(element => {
-                    arraydata[element["GAMENUM"]] = element["GAMERESULT"];
-                });
 
-                resolve(arraydata); // Resolve the JSON object directly
-            })
-            .catch((err) => {
-                console.log(err);
-                reject({"message": err});
-            });
+            db.query(`SELECT * FROM [RESULT99] WHERE CONVERT(DATE, GAMEDATE) = CONVERT(DATE, '${json["date"]}')   order by INTNUMBER;`)
+                .then((data) => {
+                    console.log("date gen res pen ");
+
+                    if (data.recordset.length > 0) {
+                        var arraydata = {};
+                        console.log(data.recordset.length);
+                        data.recordset.forEach(element => {
+                            arraydata[element["GAMENUM"]] = element["GAMERESULT"];
+                        });
+
+                        resolve(arraydata); // Resolve the JSON object directly
+                    }
+                    else
+                    {
+                        reject({"error":"please add column with todays date to retrive data from data base"});
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                    reject({ "message": err });
+                });
         });
     }
 };
