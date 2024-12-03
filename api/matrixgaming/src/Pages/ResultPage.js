5f330d2eee18db2fc93e00e1a6465f6c1c8f699f
webviosystem10 <riddhee25@gmail.com>
2024-12-03 19:07:30 +0530

    1

--- api/matrixgaming/src/Pages/ResultPage.js
+++ api/matrixgaming/src/Pages/ResultPage.js
@@ -1,8 +1,35 @@
 import React, { useState } from 'react'
+import { convertGameIdToTime, getStoneDetails } from './Globals/globalFunction';
 
 function ResultPage() {
     const [searchDate, setSearchDate] = useState();
     const [searchGame, setSearchGame] = useState();
+
+    const [stoneDetails, setStoneDetails] = useState({});
+    const [filteredData, setFilteredData] = useState([]);
+
+    useEffect(() => {
+        getStoneDetails().then((data) => {
+            setStoneDetails(data);
+        });
+
+        const getFormattedDate = (date) => {
+            const d = new Date(date);
+            const year = d.getFullYear();
+            const month = ("0" + (d.getMonth() + 1)).slice(-2);
+            const day = ("0" + d.getDate()).slice(-2);
+            return `${year}-${month}-${day}`;
+        };
+        setSearchDate(getFormattedDate(new Date()));
+    }, []);
+
+    useEffect(() => {
+        if (stoneDetails) {
+            setFilteredData(Object.values(stoneDetails));
+        }
+    }, [stoneDetails]);
+
+
     return (
         <>
             <section className='resultPage'>
@@ -26,7 +53,16 @@ function ResultPage() {
                                 <th>Bonus</th>
                             </thead>
                             <tbody>
-
+                                {filteredData
+                                    ? filteredData.map((item, index) => (
+                                        <tr key={index}>
+                                            <td className="text-center">{convertGameIdToTime(item?.gameID)}</td>
+                                            <td className="text-center">{item?.gameTime}</td>
+                                            <td className="text-center">{item?.gameResult}</td>
+                                            <td className="text-center">{item?.gameMultiplier}</td>
+                                        </tr>
+                                    ))
+                                    : "Loading..."}
                             </tbody>
                         </table>
                     </div>
