a7e0159568a243001516ed9492bd5b36772bed95
webviosystem10 <riddhee25@gmail.com>
2024-12-05 11:14:46 +0530

    1

--- api/matrixgaming/src/proxyserver.js
+++ api/matrixgaming/src/proxyserver.js
@@ -4,15 +4,19 @@ const httpProxy = require('http-proxy');
 // Create a proxy server
 const proxy = httpProxy.createProxyServer({});
 
+// Normalize host function
+const normalizeHost = (host) => {
+  if (!host) return '';
+  return host.replace(/^www\./, ''); // Remove 'www.' if it exists
+};
+
 // Main server logic
 const server = http.createServer((req, res) => {
-  const host = req.headers.host; // Get the requested domain
-
-  console.log(host)
+  const host = normalizeHost(req.headers.host); // Get the requested domain
 
   if (host === 'matrixgaming.in') {
     // Forward to the app running on port 3000
-    proxy.web(req, res, { target: 'http://77.37.47.190:80' });
+    proxy.web(req, res, { target: 'http://77.37.47.190:82' });
   } else if (host === 'jackpotresult.live') {
     // Forward to the app running on port 4000
     proxy.web(req, res, { target: 'http://77.37.47.190:81' });
